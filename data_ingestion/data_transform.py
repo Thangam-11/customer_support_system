cb60c27a5ba35a4e59a74ad3c99b522d5a9eed9b
Thangarasu <thangamani1128@gmail.com>
2025-04-26 23:17:03 +0530

    data_ingestion_commit

--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -1,14 +1,38 @@
 import pandas as pd
 from langchain_core.documents import Document
 
-
-class Data_converter:
-
+class DataConverter:
     def __init__(self):
-        pass
+        print("DataConverter initialized...")
+        self.product_data = pd.read_csv(r"C:\Users\thang\Desktop\customer_support_project\data\amazon_product_review.csv")
+        print(self.product_data.head())
 
     def data_transformation(self):
-        pass
+        required_columns = list(self.product_data.columns[1:])
+        
+        product_list = []
+        for index, row in self.product_data.iterrows():
+            product = {
+                "product_name": row['product_title'],
+                "product_rating": row['rating'],
+                "product_summary": row['summary'],
+                "product_review": row['review']
+            }
+            product_list.append(product)
+        
+        docs = []
+        for entry in product_list:
+            metadata = {
+                "product_name": entry["product_name"],
+                "product_rating": entry["product_rating"],
+                "product_summary": entry["product_summary"]
+            }
+            doc = Document(page_content=entry["product_review"], metadata=metadata)
+            docs.append(doc)
+        
+        return docs
 
-    
-    
+if __name__ == '__main__':
+    data_con = DataConverter()
+    documents = data_con.data_transformation()
+    print(documents[0])  # print one sample Document
--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 
-setup(name="e-commerce-bot",
-       version="0.0.1",
-       author="sunny",
-       author_email="snshrivas3365@gmail.com",
-       packages=find_packages(),
-       install_requires=['langchain-astradb','langchain'])
+setup(
+    name="e-commerce-bot",
+    version="0.0.1",
+    author="sunny",
+    author_email="snshrivas3365@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        'langchain-astradb',
+        'langchain'
+    ],
+)
